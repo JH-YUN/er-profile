1b1122fe018f0cef4fbb476d799e9a134f57e9e8
junhan <wnsgks789@gmail.com>
2023-05-10 00:10:36 +0900

    feat: 특성 컴포넌트 수정
    특성 추상화 수준 변경
    툴팁 컴포넌트 교체

---
+++ components/Trait.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import { useCallback } from 'react'
+import { Tooltip } from './Tooltip'
+
+interface TraitProps {
+  trait: Trait
+  size: 'main' | 'sub'
+}
+
+export const Trait = ({ trait, size }: TraitProps) => {
+  const createTooltip = useCallback((trait: Trait) => {
+    const tooltip = trait.tooltip.replace(/\{.\}/g, '?').replaceAll('\\n', ' ')
+    return (
+      <>
+        <h3 className="text-lg mb-2">{trait.name}</h3>
+        <span>{tooltip}</span>
+      </>
+    )
+  }, [])
+  const sizeClass = {
+    main: `lg:w-[45px] lg:h-[45px] w-[35px] h-[35px]`,
+    sub: `w-[35px] h-[35px] hidden xl:block`,
+  }
+  return (
+    <Tooltip message={createTooltip(trait)}>
+      <div
+        className={`bg-slate-600 border-2 rounded-full relative ${sizeClass[size]}`}
+      >
+        <Image
+          src={`/images/traits/${trait.code}.png`}
+          layout="fill"
+          alt={trait.name}
+        />
+      </div>
+    </Tooltip>
+  )
+}
--- components/Traits.tsx
+++ components/Traits.tsx
@@ -1,63 +1,28 @@
-import Image from 'next/image'
-import { useCallback } from 'react'
-import { Tooltip, TooltipWrapper, TooltipProvider } from 'react-tooltip'
-import 'react-tooltip/dist/react-tooltip.css'
+import { Trait } from './Trait'
 
 interface TraitsProps {
   firstCoreTrait: Trait
   firstSubTrait: Array<Trait>
   secondSubTrait: Array<Trait>
 }
+
 export const Traits = (props: TraitsProps) => {
   const { firstCoreTrait, firstSubTrait, secondSubTrait } = props
-  const createTooltip = useCallback((trait: Trait) => {
-    const tooltip = trait.tooltip.replace(/\{.\}/g, '?').replaceAll('\\n', ' ')
-    return `
-        <h3 class="text-lg mb-2">${trait.name}</h3>
-        <span>${tooltip}</span>
-      `
-  }, [])
-  return (
-    <TooltipProvider>
-      <div className="flex flex-col gap-2 lg:mx-[30px]">
-        <div className="flex xl:gap-1 items-end">
-          <TooltipWrapper html={createTooltip(firstCoreTrait)}>
-            <div className="bg-slate-600 border-2 rounded-full w-[35px] h-[35px] lg:w-[45px] lg:h-[45px] relative">
-              <Image
-                src={`/images/traits/${firstCoreTrait.code}.png`}
-                layout="fill"
-                alt={firstCoreTrait.name}
-              />
-            </div>
-          </TooltipWrapper>
-          {firstSubTrait?.map((trait, i) => (
-            <TooltipWrapper key={trait.code} html={createTooltip(trait)}>
-              <div className="bg-slate-600 border-2 rounded-full w-[35px] h-[35px] hidden xl:flex relative">
-                <Image
-                  src={`/images/traits/${trait.code}.png`}
-                  layout="fill"
-                  alt={trait.name}
-                />
-              </div>
-            </TooltipWrapper>
-          ))}
-        </div>
 
-        <div className="xl:flex xl:w-[123px] gap-1 items-end justify-end hidden ">
-          {secondSubTrait.map((trait) => (
-            <TooltipWrapper key={trait.code} html={createTooltip(trait)}>
-              <div className="bg-slate-600 border-2 rounded-full w-[35px] h-[35px] relative">
-                <Image
-                  src={`/images/traits/${trait.code}.png`}
-                  layout="fill"
-                  alt={trait.name}
-                />
-              </div>
-            </TooltipWrapper>
-          ))}
-        </div>
+  return (
+    <div className="flex flex-col gap-2 lg:mx-[30px]">
+      <div className="flex xl:gap-1 items-end">
+        <Trait trait={firstCoreTrait} size="main" />
+        {firstSubTrait?.map((trait, i) => (
+          <Trait trait={trait} size="sub" key={i} />
+        ))}
       </div>
-      <Tooltip className="tooltip" />
-    </TooltipProvider>
+
+      <div className="xl:flex xl:w-[123px] gap-1 items-end justify-end hidden">
+        {secondSubTrait.map((trait, i) => (
+          <Trait trait={trait} size="sub" key={i} />
+        ))}
+      </div>
+    </div>
   )
 }
