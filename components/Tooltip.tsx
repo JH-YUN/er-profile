49eb9373bbe8cb1c2f926a5c28af1ff7a192d516
junhan <wnsgks789@gmail.com>
2024-02-22 23:09:04 +0900

    fix: tooltip이 화면 밖으로 넘어가지 않도록 수정

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -1,16 +1,77 @@
-import { ReactNode } from 'react'
+'use client'
+import { ReactNode, useEffect, useRef, useState } from 'react'
 
 interface TooltipProps {
   message: string | ReactNode
   children: ReactNode
 }
 export const Tooltip = ({ message, children }: TooltipProps) => {
+  const tooltipTriggerRef = useRef<HTMLDivElement>(null)
+  const tooltipRef = useRef<HTMLDivElement>(null)
+  const [isShowTooltip, setIsShowTooltip] = useState(false)
+  const [tooltipPosition, setTooltipPosition] = useState<
+    ['left' | 'right', 'top' | 'bottom']
+  >(['right', 'bottom'])
+  const [tooltipPositionStyle, setTooltipPositionStyle] = useState<any>({
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  })
+  const tooltipWidth = 200
+
+  useEffect(() => {
+    if (tooltipTriggerRef.current) {
+      const { top, left } = tooltipTriggerRef.current.getBoundingClientRect()
+
+      if (top > window.innerHeight / 2) {
+        setTooltipPosition((prev) => [prev[0], 'top'])
+        setTooltipPositionStyle({
+          bottom: tooltipTriggerRef.current?.offsetHeight!,
+        })
+      } else {
+        setTooltipPosition((prev) => [prev[0], 'bottom'])
+        setTooltipPositionStyle({
+          top: tooltipTriggerRef.current?.offsetHeight!,
+        })
+      }
+
+      if (left + tooltipWidth > window.innerWidth) {
+        setTooltipPosition((prev) => ['left', prev[1]])
+      } else {
+        setTooltipPosition((prev) => ['right', prev[1]])
+      }
+    }
+  }, [isShowTooltip])
+
+  const handleMouseEnter = () => {
+    setIsShowTooltip(true)
+  }
+  const handleMouseLeave = () => {
+    setIsShowTooltip(false)
+  }
+
+  const tooltipWidthClass = `w-[${tooltipWidth}px]`
+
   return (
-    <div className="group relative w-fit h-fit">
+    <div
+      ref={tooltipTriggerRef}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="group relative w-fit h-fit"
+    >
       {children}
-      <div className="absolute hidden z-50  bg-zinc-800/90 text-white m-2 p-3 w-[300px] text-sm left-0 group-hover:block group-active:block">
-        {message}
-      </div>
+      {isShowTooltip && (
+        <div
+          ref={tooltipRef}
+          className={`absolute z-50 bg-zinc-800/90 text-white m-2 p-3 text-sm box-border
+          ${tooltipWidthClass}
+        `}
+          style={tooltipPositionStyle}
+        >
+          {message}
+        </div>
+      )}
     </div>
   )
 }
